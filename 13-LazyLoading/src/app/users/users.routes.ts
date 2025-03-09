f4d32f14e512d3f883a90795202a2ac105910770
YojanPawade <pawadeyojan487@gmail.com>
2025-03-09 16:07:02 +0530

    LazyLoading

---
+++ 13-LazyLoading/src/app/users/users.routes.ts
@@ -0,0 +1,37 @@
+import { Routes } from '@angular/router';
+
+import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
+import {
+  NewTaskComponent,
+  canLeaveEditPage,
+} from '../tasks/new-task/new-task.component';
+import { TasksService } from '../tasks/tasks.service';
+
+export const routes: Routes = [
+  {
+    path: '',
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+        // loadComponent: () =>
+        //   import('../tasks/tasks.component').then((mode) => mode.TasksComponent),
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage],
+      },
+    ],
+  },
+];
